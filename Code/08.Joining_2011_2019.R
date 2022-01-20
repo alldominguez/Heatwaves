8b7d665105e953e5960c91bc726b4d6246ee52f5
alldominguez <81332368+alldominguez@users.noreply.github.com>
2022-01-20 19:11:22 +0100

    actualizacion

--- Code/08.Joining_2011_2019.R
+++ Code/08.Joining_2011_2019.R
@@ -164,12 +164,99 @@ rio::export(eb_2011_2019_cardio, "eb_2011_2019_cardio.xlsx") # exportamos mortal
 rio::export(eb_2011_2019_resp, "eb_2011_2019_resp.xlsx") # exportamos mortalidad por causas respiratorias
 
 
+#############################################
+########### Santiago Centro #################
+##############################################
+
+### cargamos los datos 2017 - 2019 ###
+
+sc_2017_2019_all <- read_xlsx("SINCA_DEIS_2017_2019/sc_all_causes_sinca_2017_2019.xlsx")
+
+sc_2017_2019_cardio <- read_xlsx("SINCA_DEIS_2017_2019/sc_cardio_sinca_2017_2019.xlsx")
+
+sc_2017_2019_resp <- read_xlsx("SINCA_DEIS_2017_2019/sc_resp_sinca_2017_2019.xlsx") 
+
+c(str(sc_2017_2019_all), str(sc_2017_2019_cardio), str(sc_2017_2019_resp))
+
+colnames(sc_2017_2019_all)
+
+### cargamos los datos 2011 - 2016 ###
+
+sc_2011_2016_all <- read_xlsx("SINCA_DEIS_2011_2016/sc_all_causes_sinca_2011_2016.xlsx")
+
+sc_2011_2016_cardio <- read_xlsx("SINCA_DEIS_2011_2016/sc_cardio_sinca_2011_2016.xlsx")
+
+sc_2011_2016_resp <- read_xlsx("SINCA_DEIS_2011_2016/sc_resp_sinca_2011_2016.xlsx") 
+
+c(str(sc_2011_2016_all), str(sc_201_2016_cardio), str(sc_2011_2016_resp))
+
+colnames(sc_2011_2016_all)
+
+############# RENAME #############
+
+# modificamos el nombre de las variables, para poder unirlas 
+
+# all causes 
+colnames(sc_2011_2016_all)
+colnames(sc_2017_2019_all)
+
+sc_2011_2016_all <- sc_2011_2016_all %>% 
+  rename(temp_mean = 'Tprom', temp_max = 'Tmax', temp_min = 'Tmin', #temperature
+         pm25_mean = 'PM2.5', pm10_mean = 'PM10', o3_mean = 'ozono',  # material particulado y o3
+         ws_mean = 'VV_prom', ws_max = 'VV_max', ws_min = 'VV_min', #  velocidad del viento
+         rh_mean = 'HR_prom', rh_max = 'HR_max', rh_min = 'HR_min') # humedad relativa
+
+# cardiovascular 
+colnames(sc_2011_2016_cardio)
+colnames(sc_2017_2019_cardio)
+
+sc_2011_2016_cardio <- sc_2011_2016_cardio %>% 
+  rename(temp_mean = 'Tprom', temp_max = 'Tmax', temp_min = 'Tmin', #temperature
+         pm25_mean = 'PM2.5', pm10_mean = 'PM10', o3_mean = 'ozono',  # material particulado y o3
+         ws_mean = 'VV_prom', ws_max = 'VV_max', ws_min = 'VV_min', #  velocidad del viento
+         rh_mean = 'HR_prom', rh_max = 'HR_max', rh_min = 'HR_min') # humedad relativa
+
+# respiratory
+colnames(eb_2011_2016_resp)
+colnames(eb_2017_2019_resp)
+
+sc_2011_2016_resp <- sc_2011_2016_resp %>% 
+  rename(temp_mean = 'Tprom', temp_max = 'Tmax', temp_min = 'Tmin', #temperature
+         pm25_mean = 'PM2.5', pm10_mean = 'PM10', o3_mean = 'ozono',  # material particulado y o3
+         ws_mean = 'VV_prom', ws_max = 'VV_max', ws_min = 'VV_min', #  velocidad del viento
+         rh_mean = 'HR_prom', rh_max = 'HR_max', rh_min = 'HR_min') # humedad relativa
 
 
+###### unimos las bases ######
 
-
+sc_2011_2019_all <- dplyr::bind_rows(sc_2011_2016_all, sc_2017_2019_all) # unimos 2011 - 2019 all casuses
+sc_2011_2019_cardio <- dplyr::bind_rows(sc_2011_2016_cardio, sc_2017_2019_cardio) # unimos 2011 - 2019 cardiovascular
+sc_2011_2019_resp <- dplyr::bind_rows(sc_2011_2016_resp, sc_2017_2019_all) # unimos 2011 - 2019 respiratory
 
 
 #############################################
-########### Santiago Centro #################
+############## exportamos ################### #### el bosque ####
 ############################################
+
+rio::export(sc_2011_2019_all, "sc_2011_2019_all.xlsx") # exportamos mortalidad por todas las causas
+rio::export(sc_2011_2019_cardio, "sc_2011_2019_cardio.xlsx") # exportamos mortalidad por causas cardiovasculares
+rio::export(sc_2011_2019_resp, "sc_2011_2019_resp.xlsx") # exportamos mortalidad por causas respiratorias
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
