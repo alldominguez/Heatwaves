7c896f8d7456276a1b19f6f92c78652570411397
Alan Ignacio Domínguez Montoya <81332368+alldominguez@users.noreply.github.com>
2022-01-20 15:03:37 +0100

    actualizacion

--- Code/08.Joining_2011_2019.R
+++ Code/08.Joining_2011_2019.R
@@ -1,2 +1,9 @@
 # Combinar los datos 2011-2019 
 
+#librerias 
+pacman::p_load(tidyverse, lubridate, heatwaveR, readxl, rio, naniar, visdat, mice, patchwork)
+
+getwd() #revisamos el espacio de trabajo
+
+setwd("/Users/aldominguez/Desktop/Github_Proyectos/Heatwaves/Data") # asignamos nuestro espacio de trabajo
+ 
